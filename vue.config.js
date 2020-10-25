d732303ceb57889b08375bf1859c2001430b7d58
arily <lbsaaaaa@gmail.com>
2020-10-25 18:17:19 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,8 @@ module.exports = {
     manifestOptions: {
       background_color: '#DFE7EF'
     },
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
     workboxOptions: {
       skipWaiting: true,
       clientsClaim: true
